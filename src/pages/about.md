7a39c36fa19074008884aebaee34f0b624a20137
marcinliwen <liwen.marcin@gmail.com>
2020-04-28 18:08:58 +0200

    contakt form

--- src/pages/about.md
+++ src/pages/about.md
@@ -2,6 +2,6 @@
 title: "about"
 date: "20020-25-04"
 ---
-<p>I have been working as a front end developer since 2018, and I immediately started struggling with Smarty, PHP and SQL.</p>
+<p>I have been working as a frontend developer since 2018, and I immediately started struggling with Smarty, PHP and SQL.</p>
 <p>In the meantime I sing early music as countertenor with Polish early music ansambles.</p>
 <p>If we have the opportunity to talk on Skype, you'll see bookshelves with books behind me.</p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Contact from "../components/contact"
 
 import "../style/global.css"
 
@@ -16,13 +17,13 @@ return(
     <SEO title="Home" />
     <section id="welcome">
       <div>
-      <div dangerouslySetInnerHTML={{ __html: welcome[1]}} />
+      <div dangerouslySetInnerHTML={{ __html: welcome[0]}} />
       </div>
       <div>
         <p>follow me:</p>
         <ul className="tag-list">
-          <li className="tag">git</li>
-          <li className="tag">linkedin</li>
+          <li className="tag"><a href="https://github.com/marcinliwen">git</a></li>
+          <li className="tag"><a href="www.linkedin.com/in/marcinliwen">linkedin</a></li>
         </ul>
       </div>
     </section>
@@ -30,7 +31,7 @@ return(
     <section id="about">
       <div>
         <h2>About</h2>
-        <div dangerouslySetInnerHTML={{ __html: welcome[0]}} />
+        <div dangerouslySetInnerHTML={{ __html: welcome[1]}} />
       </div>
       
       <div>
@@ -44,10 +45,27 @@ return(
       
     </section>
     <section id="projects">
-      <h2>Projects</h2>
+      <div>
+        <h2>Projects</h2>
+      </div>
+      <div>
+        <ul>
+          <li>Butik.pl: - coding styles for the entire store based on PSD (Prestashop), - maintenance and new features</li>
+          <li>Paklo.pl: - coding styles for the entire store based on PSD (Prestashop); - Subject SG integration, - maintenance and new features</li>
+          <li>b2b.rodan24.pl: - coding styles for the entire store based on PSD (Prestashop), - maintenance and new features</li>
+          <li>Salon.pl: - coding styles for the entire store based on PSD (Prestashop), - maintenance and new features</li>
+          <li>Shoperly.pl: - coding styles for category pages based on PSD (Prestashop) - page speed optimalisation, - Inpost integration, - maintenance and new features</li>
+          <li>Sohoshop.pl: - conding style based on PSD (Wordpress)</li>
+          <li>Sas24.pl: - maintenance and new features</li>
+          <li>biuronimo.pl: - maintenance and new features</li>
+          <li>b2b.biurozplusem.pl: - maintenance and new features</li>
+        </ul>
+      </div>
+      
     </section>
     <section id="contact">
       <h2>Contact</h2>
+      <Contact />
     </section>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
@@ -63,7 +81,8 @@ export const query = graphql`
         name
       }
     },
-  allMarkdownRemark{
+  allMarkdownRemark
+  {
     edges {
       node {
         html
