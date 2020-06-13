d9c563ac3e4f129a359c639fc4db83fd3c1a36c6
marcinliwen <liwen.marcin@gmail.com>
2020-06-14 00:23:22 +0200

    remove custom hook

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,13 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React,  { useState } from "react"
-import useWindowDimensions from "./useWindowDimensions"
 
 const Header = ({ siteTitle }) =>{ 
   const [openMenu, setOpenMenu] = useState(false);
-  const [ width ] = useWindowDimensions();
+  
 
-  function openMenuHandler(){
-    if(width < 767){
+  function openMenuHandler(e){
       setOpenMenu(!openMenu)
-    }
   };
 
   return(
